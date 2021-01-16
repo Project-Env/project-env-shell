44c0205fcef1396e62adc72de65a4e01d392691c
repolevedavaj <jonas.bamberger@gmail.com>
2021-01-16 01:04:16 +0100

    introduced templating with Pebble

---
+++ src/main/java/ch/projectenv/shell/ProjectEnvShell.java
@@ -0,0 +1,55 @@
+package ch.projectenv.shell;
+
+import ch.projectenv.shell.template.TemplateProcessor;
+import ch.repolevedavaj.projectenv.core.configuration.ProjectEnvConfiguration;
+import ch.repolevedavaj.projectenv.core.configuration.ProjectEnvConfigurationFactory;
+import ch.repolevedavaj.projectenv.core.installer.ToolInstallers;
+import ch.repolevedavaj.projectenv.core.toolinfo.ToolInfo;
+import org.apache.commons.io.FileUtils;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@Command(name = "project-env-shell")
+public class ProjectEnvShell implements Callable<Integer> {
+
+    @Option(names = {"--config-file"}, required = true)
+    private File configFile;
+
+    @Option(names = {"--output-template"}, required = true)
+    private String outputTemplate;
+
+    @Option(names = {"--output-file"}, required = true)
+    private File outputFile;
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = new CommandLine(new ProjectEnvShell()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        ProjectEnvConfiguration projectEnvConfiguration = ProjectEnvConfigurationFactory.createFromFile(configFile);
+
+        File toolsDirectory = new File(projectEnvConfiguration.getToolsConfiguration().getToolsDirectory());
+
+        List<ToolInfo> toolInfos = ToolInstallers.installAllTools(projectEnvConfiguration, toolsDirectory);
+
+        writeOutput(toolInfos);
+
+        return 0;
+    }
+
+    private void writeOutput(List<ToolInfo> toolInfos) throws Exception {
+        String content = new TemplateProcessor().processTemplate(outputTemplate, toolInfos);
+
+        FileUtils.write(outputFile, content, StandardCharsets.UTF_8);
+        outputFile.setExecutable(true);
+    }
+
+}
---
+++ src/main/java/ch/projectenv/shell/template/TemplateProcessor.java
@@ -0,0 +1,76 @@
+package ch.projectenv.shell.template;
+
+import ch.repolevedavaj.projectenv.core.toolinfo.MavenInfo;
+import ch.repolevedavaj.projectenv.core.toolinfo.ToolInfo;
+import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.error.PebbleException;
+import com.mitchellbosecke.pebble.extension.AbstractExtension;
+import com.mitchellbosecke.pebble.extension.Test;
+import com.mitchellbosecke.pebble.template.EvaluationContext;
+import com.mitchellbosecke.pebble.template.PebbleTemplate;
+import org.apache.commons.lang3.ClassPathUtils;
+
+import java.io.File;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TemplateProcessor {
+
+    private final PebbleEngine pebbleEngine = new PebbleEngine
+            .Builder()
+            .extension(new ProjectEnvShellPebbleExtension())
+            .strictVariables(true)
+            .build();
+
+    public String processTemplate(String template, List<ToolInfo> toolInfos) throws Exception {
+        PebbleTemplate compiledTemplate = pebbleEngine.getTemplate(resolveTemplate(template));
+
+        Map<String, Object> context = new HashMap<>();
+        context.put("tools", toolInfos);
+
+        Writer writer = new StringWriter();
+        compiledTemplate.evaluate(writer, context);
+
+        return writer.toString();
+    }
+
+    private String resolveTemplate(String template) {
+        File templateFile = new File(template);
+        if (templateFile.exists()) {
+            return template;
+        }
+
+        return ClassPathUtils.toFullyQualifiedPath(getClass(), template);
+    }
+
+    private static class IsMavenTest implements Test {
+
+        @Override
+        public boolean apply(Object input, Map<String, Object> args, PebbleTemplate self, EvaluationContext context, int lineNumber) throws PebbleException {
+            if (input == null) {
+                throw new PebbleException(null, "Can not pass null value to \"maven\" test.", lineNumber, self.getName());
+            }
+
+            return MavenInfo.class.isAssignableFrom(input.getClass());
+        }
+
+        @Override
+        public List<String> getArgumentNames() {
+            return null;
+        }
+
+    }
+
+    private static class ProjectEnvShellPebbleExtension extends AbstractExtension {
+
+        @Override
+        public Map<String, Test> getTests() {
+            return Map.of("maven", new IsMavenTest());
+        }
+
+    }
+
+}
